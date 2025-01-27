69e13ee75fa3ec9aa6cfa66ea42f3e77e4c50d66
Chivier Humber <chivier.humber@outlook.com>
2025-01-27 19:21:34 +0000

    chore: finalize GitHub Pages build configuration

--- build_html.sh
+++ build_html.sh
@@ -9,4 +9,6 @@ d2lbook build html
 cp -r static/image _build/html/_static
 cp -r static/* _build/html/_static
 python3 tools/format_tables.py
+touch _build/html/.nojekyll
+
 cp -r _build/html _build/docs
